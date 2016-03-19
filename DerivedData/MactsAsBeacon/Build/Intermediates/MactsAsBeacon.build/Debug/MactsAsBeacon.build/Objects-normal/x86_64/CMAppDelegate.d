90dbc3e3d09a8ef7375a99629c5b53485b11dcd0
gismo141 <michael.r141@gmail.com>
2016-03-19 12:50:44 +0100

    Added binary for A1R

---
+++ DerivedData/MactsAsBeacon/Build/Intermediates/MactsAsBeacon.build/Debug/MactsAsBeacon.build/Objects-normal/x86_64/CMAppDelegate.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/michaelriedel/Developer/MactsAsBeacon/ActsAsBeacon/CMAppDelegate.m \
+  /Users/michaelriedel/Developer/MactsAsBeacon/ActsAsBeacon/CMAppDelegate.h \
+  /Users/michaelriedel/Developer/MactsAsBeacon/ActsAsBeacon/CMBeaconAdvertismentData.h
